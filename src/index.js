0210e98ca1a1c02df1ffa1233c63b4ed868d3a1f
Dmitry.Kononets <d.cononec@gmail.com>
2023-10-10 14:42:49 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,12 @@ import Daemon from './js/class/daemon';
 import Bowman from './js/class/bowman';
 import Undead from './js/class/undead';
 import Team from './js/team';
+
+const dreamteam = new Team();
+const daemon = new Daemon('Biba');
+const bowman = new Bowman('Boba');
+const undead = new Undead('Beba');
+dreamteam.addAll(daemon, bowman, undead);
+console.log(dreamteam.iterate());
+console.log(dreamteam.iterate());
+console.log(dreamteam.iterate());
